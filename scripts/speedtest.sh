a9a14a4dcd590d4e05687613dd525b362915eb3b
Derric <Derric_Lynns@comcast.com>
2019-07-20 09:36:03 +0530

    BLDK-684: Fix for licence headers in TR69 handler for IMP
    Reason for change: ST2-1269
    Test Procedure: None, license information check
    Risks: None
    Signed-off-by: Derric Lyns <Derric_Lynns@comcast.com>
    
    Change-Id: I89c033b25ba34f6b92948dcbcb8d169180ef3ff6

--- scripts/speedtest.sh
+++ scripts/speedtest.sh
@@ -1,11 +1,30 @@
 #!/bin/sh
 #
-# March 2019, derric_lynns@comcast.com
+# If not stated otherwise in this file or this component's LICENSE file the
+# following copyright and licenses apply:
 #
-# launch script for running speedtest-client this script takes care of two existing speedtest application ( the c-client and  nodejs )
+# Copyright 2019 RDK Management
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+
+#
+# description:
+# launch script for running speedtest-client this script takes care of two 
+# existing speedtest application ( the c-client and  nodejs )
 # default data is set before reading /etc/device.properties
-#
+# author: derric_lynns@comcast.com
 #
 
 # defaults
