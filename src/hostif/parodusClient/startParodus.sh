49969e92f20d7103938b9a36520551bcc7c9185e
Tony Ukken <Tony_Ukken2@cable.comcast.com>
2019-09-18 15:10:46 -0400

    RDK-25660: WebPA query should work even before activation
    Reason for change: When a device is not activated and an
    empty partner-id is returned by auth service, start parodus
    with the wildcard partner-id so that WebPA queries may work.
    Test Procedure: Verify parodus always starts up with correct
    partner-id. Verify WebPA queries work before activation.
    Risks: Low
    Signed-off-by: Tony Ukken <Tony_Ukken2@cable.comcast.com>
    
    (cherry picked from commit 9ca9195aa4d6b596c880ce844639c7f19d8ef77f)
    
    Change-Id: If7b8eccc1f7fbdf0b06e9e18c2bf2cca20504a8c
    (cherry picked from commit 4d505262c21b20c4ed69ae8d63d3eaae932cb9b7)
    
    Change-Id: I044efa1725746a0419466b566b606af0096f6cf4

--- src/hostif/parodusClient/startParodus.sh
+++ src/hostif/parodusClient/startParodus.sh
@@ -39,12 +39,10 @@ else
 fi
 
 if [ -f /usr/bin/GetConfigFile ];then
-	GetConfigFile $CONFIG_RES_FILE
+    GetConfigFile $CONFIG_RES_FILE
 else
-	echo "Error: GetConfigFile Not Found"
+    echo "Error: GetConfigFile Not Found"
 fi
-
-MAX_PARODUS_WAIT_TIME=120
 
 get_webpa_string_parameter()
 {
@@ -69,146 +67,110 @@ get_webpa_max_waiting_time()
 
 get_hardware_mac()
 {
-    hw_mac=`getEstbMacAddressWithoutColon`
-    if [ -z $hw_mac ]; then
-       timer=0
-       while :
-           do
-           if [ -e "/tmp/.macAddress" ]; then
-              hw_mac=`cat /tmp/.macAddress | sed 's/://g'`;
-              echo "$hw_mac"
-              break
-           else
-              #echo "Device details file not exists waiting for 2 min."
-              sleep 2
-              timer=`expr $timer + 2`
-              if [ $timer -eq $MAX_PARODUS_WAIT_TIME ]; then
-                 #echo "Waited for 2 min. /tmp/.macAddress does not exists, unable to start parodus"
-                 break
-              fi
-           fi
-       done
-    else  
-       echo "$hw_mac"
-    fi
-
+    hw_mac=$(getEstbMacAddressWithoutColon)
+    [ -z "$hw_mac" ] && [ -e "/tmp/.macAddress" ] && hw_mac=$(sed 's/://g' /tmp/.macAddress)
+    echo "$hw_mac"
 }
 
 get_ImageName()
 {
-  image=$(cat /version.txt | grep ^imagename:$versionTag2 | cut -d ":" -f 2)
-  echo "$image"
+    image=$(grep "^imagename:$versionTag2" /version.txt | cut -d ":" -f 2)
+    echo "$image"
 }
 
 get_SerialNumber()
 {
-   local serial
-   serial=$(grep "serial_number" /tmp/.deviceDetails.cache | cut -d'=' -f2)
+    local serial
+    serial=$(grep "serial_number" /tmp/.deviceDetails.cache | cut -d'=' -f2)
 
-   if [[ ! -z "$serial" ]]; then
-       Serial=$serial
-   else
-       serial=$(curl -d '{"paramList" : [{"name" : "Device.DeviceInfo.SerialNumber"}]}' http://127.0.0.1:10999 | rev | cut -d\" -f2 | rev)
-       if [[ -z "$serial" ]]; then
-           echo "Failed to fetch the Serial Number."
-       else
-           Serial=$serial
-       fi
+    if [ -n "$serial" ]; then
+        Serial=$serial
+    else
+        serial=$(curl -d '{"paramList" : [{"name" : "Device.DeviceInfo.SerialNumber"}]}' http://127.0.0.1:10999 | rev | cut -d\" -f2 | rev)
+        if [ -z "$serial" ]; then
+            echo "Failed to fetch the Serial Number."
+        else
+            Serial=$serial
+        fi
     fi
 }
 
 get_BootTime()
 {
-   local boot_time=$(curl -d '{"paramList" : [{"name" : "Device.DeviceInfo.X_RDKCENTRAL-COM_BootTime"}]}' http://127.0.0.1:10999 | grep -oE "[^:]+$"  | cut -d\} -f1)
-   if [[ -z "$boot_time" ]]; then
-       echo "Failed to fetch the Boot time value."
-   else
-       BootTime=$boot_time
-   fi
+    local boot_time=$(curl -d '{"paramList" : [{"name" : "Device.DeviceInfo.X_RDKCENTRAL-COM_BootTime"}]}' http://127.0.0.1:10999 | grep -oE "[^:]+$"  | cut -d\} -f1)
+    if [ -z "$boot_time" ]; then
+        echo "Failed to fetch the Boot time value."
+    else
+        BootTime=$boot_time
+    fi
 }
 
 parodus_start_up()
 {
     # Getting Webpa Parameters
-    ServerIP=`get_webpa_string_parameter "ServerIP"`
-    ACQUIRE_JWT=`get_webpa_number_parameter "acquire-jwt"`
-    if [ -z $ACQUIRE_JWT ]; then
-	ACQUIRE_JWT=1
+    ServerIP=$(get_webpa_string_parameter "ServerIP")
+    ACQUIRE_JWT=$(get_webpa_number_parameter "acquire-jwt")
+    if [ -z "$ACQUIRE_JWT" ]; then
+        ACQUIRE_JWT=1
     fi
-    NwInterface=`get_webpa_string_parameter "DeviceNetworkInterface"`
-    ServerPort=`get_webpa_number_parameter "ServerPort"`
-    PingWaitTime=`get_webpa_max_waiting_time "MaxPingWaitTimeInSec"`
-    HwMac=`get_hardware_mac`
+    NwInterface=$(get_webpa_string_parameter "DeviceNetworkInterface")
+    ServerPort=$(get_webpa_number_parameter "ServerPort")
+    PingWaitTime=$(get_webpa_max_waiting_time "MaxPingWaitTimeInSec")
+    HwMac=$(get_hardware_mac)
+    if [ -z "$HwMac" ]; then
+        echo "Failed to fetch macAddress. Exiting."
+        # sleep 10 # add any more delay needed for a macAddress fetch retry (beyond the RestartSec=10s in parodus.service)
+        exit 1
+    fi
 
     local Manufacture
-    if [[ -z ${MANUFACTURE} ]]; then
+    if [ -z "${MANUFACTURE}" ]; then
         echo "Manufacturer is undefined."
     else
         Manufacture=${MANUFACTURE}
     fi
 
     local Model
-    if [[ -z ${MODEL_NUM} ]]; then
+    if [ -z "${MODEL_NUM}" ]; then
         echo "Model number is undefined"
     else
         Model=${MODEL_NUM}
     fi
 
-    # Get Partner ID :
-    # Added Auth service query to get the Partner ID,
-    # If empty then, then again query after 2 sec interval 
-    # till max of 2 min. After max timeout, leave it empty.
-    timer=0
-    time_interval=2
+    # use partner-id from auth service to start parodus
+    if ! getDeviceIdResponse=$(curl -f -s -d POST http://127.0.0.1:50050/authService/getDeviceId); then
+        echo "Could not get Partner-id from authservice. Exiting."
+        exit 2 # Retry handled by parodus.service (Restart=always)
+    fi
+    PartnerId=$(echo $getDeviceIdResponse | cut -d\, -f2 | tr -d \} | cut -d: -f2 | tr -d \" | xargs)
+    echo "$(date) authservice returned Partner-id as \"$PartnerId\""
 
-    while :
-      do
-        param=$(curl -s -d POST http://127.0.0.1:50050/authService/getDeviceId | cut -d\, -f2 | tr -d \} | cut -d: -f2 | tr -d \" | xargs)
-        if [[ -z "$param" ]]; then
-          timer=$(( $timer + $time_interval ))
-          echo "$(date) Failed to get the Partner-id, retrying after $time_interval sec."
-          sleep $time_interval
-          if [ "$timer" = "$MAX_PARODUS_WAIT_TIME" ]; then
-            echo "$(date) Failed to get the Partner-id, retried till max time out of $MAX_PARODUS_WAIT_TIME sec."
-            break
-          fi
-       else
-         echo "$(date) Got the Partner id as \"$param"\"
-         break
-       fi
-    done
-
-    PartnerId=$param
-    if [ $PartnerId = "unknown" ]; then
-	PartnerId=""
-    else 
-	PartnerId=*,$PartnerId
+    if [ "$PartnerId" = "unknown" ]; then
+        PartnerId=""
+    else
+        PartnerId="*,$PartnerId"
     fi
 
     local reboot_reason=$(grep PreviousRebootReason /opt/logs/rebootInfo.log | cut -d: -f2)
-    if [[ -z "reboot_reason" || ' ' = $reboot_reason  ]]; then
-       echo "Last Reboot reason is not Known."
+    if [ -z "$reboot_reason" ] || [ ' ' = "$reboot_reason" ]; then
+        echo "Last Reboot reason is not Known."
     fi
 
-    Image=`get_ImageName`
+    Image=$(get_ImageName)
 
     get_SerialNumber
     get_BootTime
 
     if [ -f $CONFIG_RES_FILE ]; then
-	clientCertPath=$CONFIG_RES_FILE
+        clientCertPath=$CONFIG_RES_FILE
     else
-	echo "Failure in CFG response"
-	clientCertPath=""
+        echo "Failure in CFG response"
+        clientCertPath=""
     fi
 
-    if [ -z $HwMac ]; then
-       echo "Failed to start Parodus, Can't fetch macAddress "
-    else
-       echo "Starting parodus with arguments hw-mac=$HwMac webpa-ping-time=$PingWaitTime webpa-interface-used=$NwInterface webpa-url=$ServerIP partner-id=$PartnerId --hw-manufacturer=$Manufacture --fw-name=$Image --hw-model=$Model --hw-serial-number=$Serial --boot-time=$BootTime --hw-last-reboot-reason=$reboot_reason"
-       /bin/systemctl set-environment PARODUS_CMD=" --hw-mac=$HwMac --webpa-ping-time=$PingWaitTime --webpa-interface-used=$NwInterface --webpa-url=$ServerIP --partner-id=$PartnerId --webpa-backoff-max=9 --ssl-cert-path=$SSL_CERT_FILE  --acquire-jwt=$ACQUIRE_JWT --dns-txt-url=$DNS_TEXT_URL --jwt-public-key-file=$JWT_KEY --jwt-algo=RS256 --crud-config-file=$CRUD_CONFIG_FILE  --hw-manufacturer=$Manufacture --fw-name=$Image --hw-model=$Model --hw-serial-number=$Serial --boot-time=$BootTime --hw-last-reboot-reason=$reboot_reason --client-cert-path=$clientCertPath --token-server-url=$TOKEN_SERVER_URL"
-       echo "Parodus command set.." 
-    fi
+    echo "Starting parodus with arguments hw-mac=$HwMac webpa-ping-time=$PingWaitTime webpa-interface-used=$NwInterface webpa-url=$ServerIP partner-id=$PartnerId --hw-manufacturer=$Manufacture --fw-name=$Image --hw-model=$Model --hw-serial-number=$Serial --boot-time=$BootTime --hw-last-reboot-reason=$reboot_reason"
+    /bin/systemctl set-environment PARODUS_CMD=" --hw-mac=$HwMac --webpa-ping-time=$PingWaitTime --webpa-interface-used=$NwInterface --webpa-url=$ServerIP --partner-id=$PartnerId --webpa-backoff-max=9 --ssl-cert-path=$SSL_CERT_FILE  --acquire-jwt=$ACQUIRE_JWT --dns-txt-url=$DNS_TEXT_URL --jwt-public-key-file=$JWT_KEY --jwt-algo=RS256 --crud-config-file=$CRUD_CONFIG_FILE  --hw-manufacturer=$Manufacture --fw-name=$Image --hw-model=$Model --hw-serial-number=$Serial --boot-time=$BootTime --hw-last-reboot-reason=$reboot_reason --client-cert-path=$clientCertPath --token-server-url=$TOKEN_SERVER_URL"
+    echo "Parodus command set.."
 }
 
 parodus_start_up
