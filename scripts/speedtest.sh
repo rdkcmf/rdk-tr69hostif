b70f569a5705a2cfb754bd77165972ac97a753fc
karthikeyan shanmugam <karthikeyan_shanmugam@comcast.com>
2020-10-21 14:17:34 +0530

    RDKB-19277, ST2-10150 : Remove Node client support, clean up script
    Reason for change: Timestamp added for the log messages. NodeJS support removed for XI6.
    Test Procedure: Run the speedtest.sh script on XI6 device.
    Verify the timestamp in the log file /opt/logs/speedtest-init.log
    Risks: None
    Signed-off-by: Karthik <karthikeyan_shanmugam@comcast.com>
    
    (cherry picked from commit b5a36ebf89df64562045362760aa1ce84abd05f0)
    (cherry picked from commit 482d7718220ed42b5d407e7b94f28339f08dd747)
    Change-Id: If06d5e1b9b576fea7aa7fdeadbc5c80d51c43847

--- scripts/speedtest.sh
+++ scripts/speedtest.sh
@@ -21,19 +21,19 @@
 
 #
 # description:
-# launch script for running speedtest-client this script takes care of two 
-# existing speedtest application ( the c-client and  nodejs )
+# launch script for running speedtest-client this script support
+# existing speedtest application ( the c-client ) only.
+# NodeJS support is removed.
 # default data is set before reading /etc/device.properties
 # author: derric_lynns@comcast.com
 #
 
 # defaults
 SPDTST_NAME="speedtest-client"
-NODE_SPDTST_NAME="run_speedtest.sh"
 SPDTST_PATH=/usr/bin
-NODE_SPDTST_PATH=/etc/speedtest
 SPDTST_DBG_PATH=/opt/diagnostics
 SPDTST_LOGPATH=/opt/logs
+LOG_FILE=/opt/logs/speedtest-init.log
 
 # make sure the below logfile name is same as in log4c
 SPDTST_LOGFILE=speedtest_log.txt
@@ -42,7 +42,7 @@ SPDTST_DEVICE="XI6"
 SPDTST_CLIENT_TYPE=1
 
 # uncomment the following lines for diagnostics mode launch
-# echo "diagnostics launch"
+# echo "$(date +"[%Y-%m-%d %H:%M:%S]" )  diagnostics launch" >> $LOG_FILE
 # if debug path exists then take it from there
 # if [ -d "$SPDTST_DBG_PATH" ]; then
 #    SPDTST_PATH="$SPDTST_DBG_PATH"
@@ -74,12 +74,10 @@ fi
 
 # fuel the speedtest
 SPDTST_BIN=${SPDTST_PATH}/${SPDTST_NAME}
-NODE_SPDTST_BIN=${NODE_SPDTST_PATH}/${NODE_SPDTST_NAME}
 SPDTST_LOG=${SPDTST_LOGPATH}/${SPDTST_LOGFILE}
 
 # launch script and log to file
 SPDTST_LAUNCH_CMD="${SPDTST_BIN} &"
-SPDTST_NODEJS_CMD="${NODE_SPDTST_BIN} &"
 
 # decide client type for technicolor or arris XI6
 if [ "$SPDTST_DEVICE" = "XI6" ]; then
@@ -92,21 +90,17 @@ else # rest of the devices on dmcli
 fi
 
 # default and set, prepare to launch
-LAUNCH_CMD=${SPDTST_NODEJS_CMD}
+LAUNCH_CMD=${SPDTST_LAUNCH_CMD}
 
 # get the client type and fall back on command/cli error
 SPDTST_CLIENT_TYPE=$(eval "$TYPE_CMD")
-nummer='^[0-9]+$'
-
-if ! [[ $SPDTST_CLIENT_TYPE =~ $nummer ]] ; then
-   SPDTST_CLIENT_TYPE=1
-fi
 
 if [ "$SPDTST_CLIENT_TYPE" = "1" ]; then
     LAUNCH_CMD=${SPDTST_LAUNCH_CMD}
+    # launch now
+    echo "$(date +"[%Y-%m-%d %H:%M:%S]" ) launching speedtest" >> $LOG_FILE
+    echo "$(date +"[%Y-%m-%d %H:%M:%S]" ) $LAUNCH_CMD " >> $LOG_FILE
+    $(eval "$LAUNCH_CMD")
+else
+    echo "$(date +"[%Y-%m-%d %H:%M:%S]" ) Unsupported client for XI6" >> $LOG_FILE
 fi
-
-# launch now
-echo "launching speedtest" > /dev/console
-echo $LAUNCH_CMD > /dev/console
-$(eval "$LAUNCH_CMD")
